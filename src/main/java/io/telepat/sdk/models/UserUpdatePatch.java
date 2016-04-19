8e169970faffbacae9981cfa5bc010d5577a107c
Roxana Cristea <roxana@appscend.com>
2016-04-19 19:59:10 +0300

    Added constructors to UserUpdatePatch for easier instatiation

--- src/main/java/io/telepat/sdk/models/UserUpdatePatch.java
+++ src/main/java/io/telepat/sdk/models/UserUpdatePatch.java
@@ -4,6 +4,14 @@ package io.telepat.sdk.models;
  * Created by Andrei Marinescu on 11/29/15.
  */
 public class UserUpdatePatch {
+
+    public UserUpdatePatch() {}
+
+    public UserUpdatePatch(String fieldName, Object fieldValue) {
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
     public String getFieldName() {
         return fieldName;
     }
